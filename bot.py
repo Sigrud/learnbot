71d6e9fa9fc4c98e46847a56fab695eb73d58228
Sergey Osotov <sindik63@gmail.com>
2018-12-01 01:39:57 +0700

    1 and 2 lessons done

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 from telegram.ext import Updater, CommandHandler,MessageHandler , Filters
 import logging
 import settings
+import ephem, datetime
+
+date=datetime.datetime.now()
+today=date.strftime('%Y-%m-%d')
 
 
 
@@ -12,6 +16,7 @@ def greet_user(bot,update):
 	text='вызван  /start'
 	logging.info(text)
 	update.message.reply_text(text)
+	print(update.message.text)
 
 def  talk_to_me(bot,update):
 	user_text = "Привет {} Ты написал : {}".format(update.message.chat.first_name,
@@ -19,6 +24,20 @@ def  talk_to_me(bot,update):
 	logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username,
 		update.message.chat.id, update.message.text)
 	update.message.reply_text(user_text)
+
+
+
+def talk_about_planet(bot,update):
+	
+	logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username,
+		update.message.chat.id, update.message.text)
+	user_text = (update.message.text)
+	planets=user_text.split()
+	name_pl=(planets[1].strip()).title()
+	pl=getattr(ephem,name_pl)(today)	
+	update.message.reply_text(f'Планета {name_pl} \
+		находится в созвездии \n{ephem.constellation(pl)}')
+	
 
 
 def main():
@@ -30,6 +49,7 @@ def main():
 	dp = mybot.dispatcher
 	dp.add_handler(CommandHandler('start',greet_user))
 	dp.add_handler(MessageHandler(Filters.text,talk_to_me))
+	dp.add_handler(CommandHandler('planet',talk_about_planet))
 
 	mybot.start_polling()
 	mybot.idle()
